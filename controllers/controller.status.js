1eae07c90ee50cb0c484450d8a9324f8fce227e5
Adam Albright <aalbright425@gmail.com>
2017-11-15 15:49:26 -0500

    Css positions canvases accordingly on mobile

--- controllers/controller.status.js
+++ controllers/controller.status.js
@@ -1,4 +1,5 @@
 
+/*The Controller for Status*/
 
 'use strict';
 
@@ -12,11 +13,14 @@ exports.getModels = function (req, res) {
     };
 
     console.log('Models Requested');
+    // Get the models from the address specified in options.
     http.get(options, function(resp) {
         let data = '';
+
         resp.on('data', function(chunk) {
             data += chunk;
         });
+
         resp.on('end', function(){
             console.log(data);
             res.setHeader('Content-Type', 'application/json');
--- public/js/getModels.js
+++ public/js/getModels.js
@@ -1,4 +1,7 @@
-
+/*
+httpGetModels makes a get request to the neural network manager to for the models available.
+One important note is that cors has to be enabled for the address the get request is being sent to.
+* */
 function httpGetModels(targetURL)
 {
     let xhr = new XMLHttpRequest(),
@@ -7,10 +10,15 @@ function httpGetModels(targetURL)
 
     xhr.open(method, url, true);
     xhr.onreadystatechange = function () {
+        // When the request is complete and if it was a success, get the models and
         if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+            // Parse the response into a json object.
             let models = JSON.parse(xhr.responseText);
-            let selector = document.getElementById('model_selection')
+            // Get the selector in the document
+            let selector = document.getElementById('model_selection');
             console.log(models[0]);
+            // Split the keys and values. Use the keys as the text for the selector and
+            // use the json value at the selector value.
             for (let key in models[0]){
                 let value = models[0][key];
                 console.log(key + ' : ' + value);
@@ -20,7 +28,7 @@ function httpGetModels(targetURL)
                 opt.appendChild(text);
                 selector.appendChild(opt);
             }
-            //selector.appendChild()
+
         }
     };
 
